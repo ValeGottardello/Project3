7dbd9255fe3d67ef2e97fea2568494dc67d5f0a9
utamakevin <utama.kevin@yahoo.com>
2023-04-03 12:38:22 +0700

    map is centered on sydney downtown, zoom default at 13 and minimum zoom is 10

--- public/client.js
+++ public/client.js
@@ -2,8 +2,9 @@ let map = { lat: -33.8712, long: 151.2046 }
 
 async function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: { lat: -33.87995, lng: 151.200886 },
+    zoom: 13,
+    minZoom: 10,
   })
 }
 
