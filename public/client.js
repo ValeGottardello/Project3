4e843a7d01b66057e537e2672f055f68f21ba962
utamakevin <utama.kevin@yahoo.com>
2023-04-03 13:30:58 +0700

    commit to rebase

--- public/client.js
+++ public/client.js
@@ -6,4 +6,19 @@ async function initMap() {
     zoom: 13,
     minZoom: 10,
   })
+
+  infoWindow = new google.maps.InfoWindow()
+
+  const currentLocation = document.getElementById('current-location')
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      }
+      currentLocation.innerHTML = `${position.coords.latitude},  ${position.coords.longitude}`
+    })
+  }
+
 }
