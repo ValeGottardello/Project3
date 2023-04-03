316759c92cbfb2d66b3cca8a1592840fb4953186
ValeGottardello <valengottardello@hotmail.com>
2023-04-03 15:52:20 +1000

    adding route oathe to fetch all stations

---
+++ models/stations.js
@@ -0,0 +1,11 @@
+const db = require('../config')
+
+class Station {
+  static findAll() {
+    return db.query('select * from stations order by id asc;').then((res) => {
+      return res.rows.slice(0, 400)
+    })
+  }
+}
+
+module.exports = Station
