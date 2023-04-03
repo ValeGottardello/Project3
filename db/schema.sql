7a5f79976473c613bfea18958d0bc02b14e4ce8e
dimitrinossar <dimitri.nossar@gmail.com>
2023-04-03 16:29:05 +1000

    fixed db schema, fixed Owners model

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ CREATE DATABASE stations_app;
 CREATE TABLE stations(
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
+  owner TEXT NOT NULL,
   address TEXT NOT NULL,
   suburb TEXT NOT NULL,
   state TEXT NOT NULL,
