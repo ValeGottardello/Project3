eac33e4f42a84e6bbd927e4bd75fa1d41767b75a
dimitrinossar <dimitri.nossar@gmail.com>
2023-04-03 14:56:25 +1000

    added db schema and seed script

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE stations_app;
+
+CREATE TABLE stations(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  address TEXT NOT NULL,
+  suburb TEXT NOT NULL,
+  state TEXT NOT NULL,
+  latitude TEXT NOT NULL,
+  longitude TEXT NOT NULL
+);
---
+++ db/seed_database.js
@@ -0,0 +1,27 @@
+const db = require('../config')
+const fs = require('fs')
+
+let stations = fs.readFileSync('./resources/stations.csv', 'utf-8')
+
+stations
+  .split('\n')
+  .slice(1)
+  .map((station) => {
+    const values = station.split(',')
+    return [
+      values[7],
+      values[9],
+      values[10],
+      values[11],
+      values[15],
+      values[16],
+    ]
+  })
+  .forEach((station) => {
+    const sql = `
+      INSERT INTO
+      stations (name, address, suburb, state, latitude, longitude)
+      VALUES ($1, $2, $3, $4, $5, $6);
+    `
+    db.query(sql, station)
+  })
