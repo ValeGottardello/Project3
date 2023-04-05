5a0b796372deb149964dbbb7e4c19344f994e628
dimitrinossar <dimitri.nossar@gmail.com>
2023-04-06 01:13:47 +1000

    fixed sidebar shortcut bug caused by my css

--- public/components/shortcut.js
+++ public/components/shortcut.js
@@ -1,24 +1,28 @@
 const columnRight = document.getElementById('right')
 const columnLeft = document.getElementById('left')
-const map = document.getElementById('map')
+const wrapper = document.getElementById('main-wrapper')
 
 function handleShortCut(evt) {
   if (evt.ctrlKey && evt.key === 'b') {
     evt.preventDefault()
 
-    if (
-      columnRight.style.display === 'none' &&
-      columnLeft.style.display === 'none' &&
-      map.style.position !== 'relative'
-    ) {
-      columnRight.style.display = 'block'
-      columnLeft.style.display = 'block'
-      map.style.position = 'relative'
-    } else {
-      columnRight.style.display = 'none'
-      columnLeft.style.display = 'none'
-      map.style.position = 'absolute'
-    }
+    // if (
+    //   columnRight.style.display === 'none' &&
+    //   columnLeft.style.display === 'none' &&
+    //   map.style.position !== 'relative'
+    // ) {
+    //   columnRight.style.display = 'block'
+    //   columnLeft.style.display = 'block'
+    //   map.style.position = 'relative'
+    // } else {
+    //   columnRight.style.display = 'none'
+    //   columnLeft.style.display = 'none'
+    //   map.style.position = 'absolute'
+    // }
+
+    columnRight.classList.toggle('hidden')
+    columnLeft.classList.toggle('hidden')
+    wrapper.classList.toggle('expanded')
   }
 }
 
