85ea0b47bfc1ae3cdad943caca4f6a41a4af727f
utamakevin <utama.kevin@yahoo.com>
2023-04-05 11:11:44 +0700

    added distance on nearby stations, added stats css

--- public/components/nearest_stations.js
+++ public/components/nearest_stations.js
@@ -9,13 +9,32 @@ export function renderEachStation(stations) {
 }
 
 function renderListStations(station) {
+  let [currentLat, currentLong] = String(map.getCenter())
+    .slice(1, -1)
+    .split(', ')
+
+  const R = 6371e3 // metres
+  const φ1 = (currentLat * Math.PI) / 180 // φ, λ in radians
+  const φ2 = (station.latitude * Math.PI) / 180
+  const Δφ = ((station.latitude - currentLat) * Math.PI) / 180
+  const Δλ = ((station.longitude - currentLong) * Math.PI) / 180
+
+  const a =
+    Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  const distance = R * c // in metres
+
+  const roundedDist = String(distance).split('.')[0]
+
   return `
-    <article data-id="${station.id}" class="station">
-        <p>${station.name}</p>
-        <p>${station.address}</p>
-        <p>${station.owner}</p>
-    </article>
-    `
+  <article data-id="${station.id}" class="station">
+    <p>${station.name} <b>${roundedDist}m</b></p>
+    <p>${station.address}</p>
+    <p>${station.owner}</p>
+  </article>
+  `
 }
 
 export function loadNearStations() {
