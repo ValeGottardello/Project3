841df82e6202d579f42889b9af30f95ceb79f91b
dimitrinossar <dimitri.nossar@gmail.com>
2023-04-06 00:03:34 +1000

    direction renderer is a little more robust now

--- public/directions_service.js
+++ public/directions_service.js
@@ -4,20 +4,20 @@ const nearestTable = document.getElementById('nearest-placeholder')
 directionsRenderer.setMap(map)
 
 function calcRoute(address) {
-  var request = {
+  const request = {
     origin: map.getCenter(),
     destination: address,
     travelMode: 'DRIVING',
   }
-  directionsService.route(request, function (result, status) {
-    if (status == 'OK') {
-      directionsRenderer.setDirections(result)
-    }
+  directionsService.route(request, (result) => {
+    directionsRenderer.setDirections(result)
   })
 }
 
 nearestTable.addEventListener('click', (evt) => {
   evt.preventDefault()
-  const address = evt.target.closest('.address').textContent
+  const directionSelector = evt.target.closest('.nearby-main')
+  if (!directionSelector) return
+  const address = directionSelector.querySelector('.address').textContent
   calcRoute(address)
 })
