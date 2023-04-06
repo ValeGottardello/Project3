c9bb950ab9e3980c5542a4af983935db90ec3d49
ValeGottardello <valengottardello@hotmail.com>
2023-04-06 11:01:41 +1000

    coimmit to rebase

--- public/directions_service.js
+++ public/directions_service.js
@@ -2,6 +2,7 @@ const directionsService = new google.maps.DirectionsService()
 const directionsRenderer = new google.maps.DirectionsRenderer()
 const nearestTable = document.getElementById('nearest-placeholder')
 directionsRenderer.setMap(map)
+const refreshDirect = document.querySelector('.reset')
 
 function calcRoute(address) {
   const request = {
@@ -9,8 +10,15 @@ function calcRoute(address) {
     destination: address,
     travelMode: 'DRIVING',
   }
-  directionsService.route(request, (result) => {
-    directionsRenderer.setDirections(result)
+  directionsService.route(request, (result, status) => {
+    if (status == 'OK') {
+      directionsRenderer.setDirections(result)
+    } else if (status == 'NOT_FOUND' || status == 'ZERO_RESULTS') {
+      directionsRenderer.setDirections(null)
+      let message = document.createElement('p')
+      message.textContent = 'Place not found, try it again'
+      message.appendChild(address)
+    }
   })
 }
 
@@ -20,4 +28,10 @@ nearestTable.addEventListener('click', (evt) => {
   if (!directionSelector) return
   const address = directionSelector.querySelector('.address').textContent
   calcRoute(address)
+})
+
+refreshDirect.addEventListener('click', () => {
+  directionsRenderer.setMap(null)
+  map.setCenter(map.getCenter())
+  map.setZoom(13)
 })
